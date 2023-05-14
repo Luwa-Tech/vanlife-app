cd028cc6964d4a4c4ad4502303783067d69548e0
yusuf umar faruq <luwaservices@gmail.com>
2023-05-13 22:44:12 -0700

    fixed error when fetching data

--- src/Pages/Vans.jsx
+++ src/Pages/Vans.jsx
@@ -1,25 +1,23 @@
 import React, { useEffect, useState } from "react"
 
-
 const Vans = () => {
     const [vans, setVans] = React.useState([])
 
     React.useEffect(() => {
         const fetchData = async () => {
-            setTimeout(async () => {
                  const response = await fetch("/api/vans")
                  const data = await response.json()
-        
-                 return data
-             }, 2000)
-        
-              console.log(data)
+
+            setVans(data.vans)
            } 
         
            fetchData().catch(err => console.log(err))
     }, [])
 
+    console.log(vans)
+
     return <h1>Vans page goes here</h1>
 }
 
 export default Vans
+
